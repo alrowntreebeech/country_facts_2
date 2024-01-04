418112f36951890582b4b2c2df650f76b440f29f
Alex Rowntree-Beech <118824878+alrowntreebeech@users.noreply.github.com>
2024-01-04 16:16:09 +0000

    Create Home Page

---
+++ src/components/homePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+const HomePage = () => {
+    return (
+        <Container fluid className="bg-black flex-fill">
+        </Container>
+    )
+};
+
+export default HomePage;
