1b4f13daa2c5f052ca3fa9559790f153e6dc0a4c
Alex Rowntree-Beech <118824878+alrowntreebeech@users.noreply.github.com>
2024-01-05 15:28:59 +0000

    Test threejs

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -1,9 +1,16 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import { Canvas } from '@react-three/fiber';
 
 const HomePage = () => {
     return (
         <Container fluid className="bg-black flex-fill">
+            <Canvas>
+                <mesh>
+                    <sphereGeometry  />
+                    <meshBasicMaterial />
+                </mesh>
+            </Canvas>
         </Container>
     )
 };
